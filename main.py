6290897d856b2a141c393d8aed4225675908e7c1
maciekadamczykk <maciek.adamczyk2004@gmail.com>
2025-03-20 18:36:19 +0100

    Splitting the data into train set and test set

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv('LifeExpectancy.csv')
 
-print(dataset.head())
+
+train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
+
+print(f"Training set size: {train_dataset.shape}")
+print(f"Testing set size: {test_dataset.shape}")
